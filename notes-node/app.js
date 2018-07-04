f70fa8fee29cbc2774714805566fe5c5fdba5a6e
keza <dk@citadel.rs>
2018-07-04 20:19:11 +0200

    Use yargs to read command parameters. Prepare functions for CRUD.

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,5 +2,24 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes');
+
+const argv = yargs.argv;
+let command = argv._[0];
+console.log(command);
+console.log("process.argv", process.argv);
+console.log("yargs.argv", argv);
+
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read') {
+    notes.readNote(argv.title);
+} else if (command === 'remove') {
+    notes.removeNote(argv.title);
+} else {
+    console.log('Unknown');
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,6 +1,24 @@
 console.log("Starting notes.js");
 
-module.exports.AddNote = () => {
-    console.log('addNote');
-    return "New note";
+let addNote = (title, body) => {
+    console.log('Adding note', title, body);
 }
+
+let getAll = (title, body) => {
+    console.log('Get all');
+}
+
+let readNote = (title) => {
+    console.log('Read note', title);
+}
+
+let removeNote = (title) => {
+    console.log('Remove note', title);
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    readNote,
+    removeNote
+};
