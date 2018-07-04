34c0a5b556f6d31ba1bdf4c6e4efe28c73550f3a
keza <dk@citadel.rs>
2018-07-04 18:13:55 +0200

    Add lodash module

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,10 +2,17 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes');
 
-let res = notes.AddNote();
-console.log(res);
+let fArray = _.uniq(['Mike',1,2,3,'Mike',3])
+
+console.log(fArray);
+
+// console.log(_.isString(true));
+// console.log(_.isString("x"));
+//let res = notes.AddNote();
+//console.log(res);
 // let user = os.userInfo();
 
 // fs.appendFileSync('greeting.txt', `Hello ${user.username}! You are ${notes.age}`);
