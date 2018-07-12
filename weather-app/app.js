f84a7dd189b7136879f3e6dede2662d66f486a83
keza <dk@citadel.rs>
2018-07-12 20:26:37 +0200

    Add proxy. Get address, lat and lng.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,13 @@
 const request = require('request');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 request({
     url : 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
-    json: true
+    json: true,
+    proxy:'http://127.0.0.1:8888'
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
-})
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+});
