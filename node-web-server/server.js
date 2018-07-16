b0708d202e92af6f9f3b16cdf1306c2e83924d97
keza <dk@citadel.rs>
2018-07-16 23:38:41 +0200

    Partials and helper functions

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,13 +3,22 @@ const hbs = require('hbs');
 let app = express();
 let port = 3000;
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
     res.render("home.hbs", {
         pageTitle: 'Home Page',
-        currentYear: new Date().getFullYear(),
         welcomeMessage: 'Hi, welcome to my page!'
     });
 });
@@ -17,7 +26,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render("about.hbs", {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
     });
 })
 
