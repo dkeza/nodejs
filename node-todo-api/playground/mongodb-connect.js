7b87158b751d2db70b2ebe3355be42b26da0888d
keza <dk@citadel.rs>
2018-07-21 00:09:37 +0200

    Initial commit

---
+++ node-todo-api/playground/mongodb-connect.js
@@ -0,0 +1,23 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        console.log('Unable to connect to MongoDB server');
+        return;
+    }
+    console.log("Connected to MongoDB server");
+
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    }, (err, result) => {
+        if (err) {
+            console.log('Unable to insert');
+            return;
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    db.close();
+});
